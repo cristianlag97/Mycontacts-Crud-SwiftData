81ac6586db76ea0e8b144dcbfdbbf5e202678f97
Cristian Laguna <cristiandavidlaguna.97@gmail.com>
2024-04-28 18:25:30 -0500

    Initial Commit

---
+++ MyContacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyContacts
+//
+//  Created by Cristian David Laguna Aldana on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by Cristian David Laguna Aldana on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
